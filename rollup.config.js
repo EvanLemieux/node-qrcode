5b08f6a8ef44912f2df0b2b58d25928f11bcb6a6
Evan Lemieux <49823444+EvanLemieux@users.noreply.github.com>
2023-10-02 16:33:27 -0500

    Clean up repo for browser use

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,19 @@
-import babel from 'rollup-plugin-babel'
-import { terser } from 'rollup-plugin-terser'
-import commonjs from '@rollup/plugin-commonjs'
-import resolve from '@rollup/plugin-node-resolve'
+import babel from 'rollup-plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
 
 const babelConfig = {
-  babelrc: false,
-  presets: [['@babel/preset-env', { targets: 'defaults, IE >= 10, Safari >= 5.1' }]]
-}
+	babelrc: false,
+	presets: [['@babel/preset-env', { targets: 'defaults, IE >= 10, Safari >= 5.1' }]]
+};
 
 export default [{
-  input: 'lib/browser.js',
-  output: { file: 'build/qrcode.js', format: 'iife', name: 'QRCode', exports: 'named' },
-  plugins: [commonjs(), resolve(), babel(babelConfig), terser()]
+	input: 'lib/browser.js',
+	output: { file: 'build/qrcode.js', format: 'iife', name: 'QRCode', exports: 'named' },
+	plugins: [commonjs(), resolve(), babel(babelConfig), terser()]
 }, {
-  input: 'helper/to-sjis-browser.js',
-  output: { file: 'build/qrcode.tosjis.js', format: 'iife', exports: 'none' },
-  plugins: [commonjs(), resolve(), babel(babelConfig), terser()]
-}]
+	input: 'helper/to-sjis-browser.js',
+	output: { file: 'build/qrcode.tosjis.js', format: 'iife', exports: 'none' },
+	plugins: [commonjs(), resolve(), babel(babelConfig), terser()]
+}];
--- test/unit/helper/sjis.test.js
+++ test/unit/helper/sjis.test.js
@@ -1,18 +1,18 @@
-const test = require('tap').test
-const toSJIS = require('helper/to-sjis')
+const test = require('tap').test;
+const toSJIS = require('helper/to-sjis');
 
-test('SJIS from char', function (t) {
-  t.notOk(toSJIS(''),
-    'Should return undefined if character is invalid')
+test('SJIS from char', (t) => {
+	t.notOk(toSJIS(''),
+		'Should return undefined if character is invalid');
 
-  t.notOk(toSJIS('A'),
-    'Should return undefined if character is not a kanji')
+	t.notOk(toSJIS('A'),
+		'Should return undefined if character is not a kanji');
 
-  t.equal(toSJIS('襦'), 0xe640,
-    'Should return correct SJIS value')
+	t.equal(toSJIS('襦'), 0xe640,
+		'Should return correct SJIS value');
 
-  t.equal(toSJIS('￢'), 0x81ca,
-    'Should return correct SJIS value')
+	t.equal(toSJIS('￢'), 0x81ca,
+		'Should return correct SJIS value');
 
-  t.end()
-})
+	t.end();
+});
